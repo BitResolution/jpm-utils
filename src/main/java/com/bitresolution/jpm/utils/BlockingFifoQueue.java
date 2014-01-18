361f63cee8604f0d8ce0c716088c97eca74a4cc2
James McGivern <bitresolution-github@sorrynoremorse.oib.com>
2014-01-18 20:54:19 +0000

    First pass on implementing FIFO queue

--- src/main/java/com/bitresolution/jpm/utils/BlockingFifoQueue.java
+++ src/main/java/com/bitresolution/jpm/utils/BlockingFifoQueue.java
@@ -1,13 +1,34 @@
 package com.bitresolution.jpm.utils;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BlockingFifoQueue<T> implements FifoQueue<T> {
 
+
+    private final Queue<T> queue;
+    private final int capacity;
+
+    public BlockingFifoQueue(int capacity) {
+        this.capacity = capacity;
+        this.queue = new LinkedList<T>();
+    }
+
     @Override
-    public synchronized void enqueue(T o) {
+    public synchronized void enqueue(T item) {
+        queue.add(item);
     }
 
     @Override
     public synchronized T dequeue() {
         return null;
+    }
+
+    public int size() {
+        return queue.size();
+    }
+
+    public int getCapacity() {
+        return capacity;
     }
 }
--- src/test/java/com/bitresolution/jpm/utils/BlockingFifoQueueTest.java
+++ src/test/java/com/bitresolution/jpm/utils/BlockingFifoQueueTest.java
@@ -1,5 +1,25 @@
 package com.bitresolution.jpm.utils;
 
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.lessThanOrEqualTo;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assume.assumeThat;
+
 public class BlockingFifoQueueTest {
 
+    @Test
+    public void shouldBeAbleToAddElementToUnderCapacityQueue() {
+        //given
+        BlockingFifoQueue<String> queue = new BlockingFifoQueue<String>(1);
+        assumeThat(queue.size(), is(0));
+        assumeThat(queue.getCapacity(), is(1));
+        assumeThat(queue.size(), is(lessThanOrEqualTo(queue.getCapacity())));
+        //when
+        queue.enqueue("item");
+        //then
+        assertThat(queue.size(), is(1));
+        assertThat(queue.getCapacity(), is(1));
+    }
 }
