32e41d365f08a4e1c74199e2f5490c2c69cfdc19
James McGivern <bitresolution-github@sorrynoremorse.oib.com>
2014-01-19 12:32:27 +0000

    Created failing test for dequeuing from an empty blocking queue

--- src/test/java/com/bitresolution/jpm/utils/BlockingFifoQueueTest.java
+++ src/test/java/com/bitresolution/jpm/utils/BlockingFifoQueueTest.java
@@ -1,7 +1,14 @@
 package com.bitresolution.jpm.utils;
 
+import com.jayway.awaitility.Duration;
 import org.junit.Test;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import static com.jayway.awaitility.Awaitility.await;
+import static com.jayway.awaitility.Awaitility.to;
 import static org.hamcrest.Matchers.lessThanOrEqualTo;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -39,5 +46,85 @@ public class BlockingFifoQueueTest {
         //then
         assertThat(item, is(ITEM_A));
         assertThat(queue.size(), is(1));
+    }
+
+    @Test
+    public void shouldBlockUntilElementIsAvailableWhenRemovingFromQueue() {
+        //given
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+        BlockingFifoQueue<String> queue = new BlockingFifoQueue<String>(1);
+        Producer producer = new Producer(queue, ITEM);
+        Consumer consumer = new Consumer(queue);
+        assumeThat(consumer.isComplete(), is(false));
+
+        //when
+        scheduler.execute(consumer);
+        //out of order execution means unless we block until consumer.isRunning we might check the state too early
+        //if the queue is not actually blocking on dequeues!
+        await().untilCall(to(consumer).isRunning(), is(true));
+        assertThat(consumer.isComplete(), is(false));
+
+        scheduler.schedule(producer, Duration.ONE_SECOND.getValueInMS(), TimeUnit.MILLISECONDS);
+
+        //then
+        await().untilCall(to(producer).isComplete(), is(true));
+        await().atMost(Duration.FIVE_SECONDS).untilCall(to(consumer).isComplete(), is(true));
+    }
+
+
+    private static class Producer implements Runnable {
+
+        private final BlockingFifoQueue<String> queue;
+        private final String item;
+        private boolean complete;
+
+        private Producer(BlockingFifoQueue<String> queue, String item) {
+            this.queue = queue;
+            this.item = item;
+            this.complete = false;
+        }
+
+        @Override
+        public void run() {
+            synchronized (this) {
+                queue.enqueue(item);
+                this.complete = true;
+            }
+        }
+
+        public boolean isComplete() {
+            return complete;
+        }
+    }
+
+    private static class Consumer implements Runnable {
+
+        private final BlockingFifoQueue<String> queue;
+        private String item;
+        private boolean complete;
+        private boolean running;
+
+        private Consumer(BlockingFifoQueue<String> queue) {
+            this.queue = queue;
+            this.complete = false;
+        }
+
+        @Override
+        public void run() {
+            synchronized (this) {
+                this.running = true;
+                this.item = queue.dequeue();
+                this.complete = true;
+            }
+        }
+
+        public boolean isComplete() {
+            return complete;
+        }
+
+        public boolean isRunning() {
+            return running;
+        }
     }
 }
